7c3999cb7a55f06a4e1165c0392507466b6631c5
sutigit <panyanilben@gmail.com>
2024-11-01 19:22:57 +0200

    update package.json

---
+++ build/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Main {
+    constructor(word) {
+        this.word = word;
+    }
+    getWord() {
+        return this.word;
+    }
+}
+exports.default = Main;
